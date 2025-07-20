3053e907d5f25fa0b580429fa8bbe6445cde9272
YutaoShao <2423271612@qq.com>
2025-07-20 15:31:29 +0800

    feat: 新增员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,15 +12,20 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeMapper employeeMapper;
+    @Autowired
+    private EmployeeService employeeService;
 
     /**
      * 员工登录
@@ -39,13 +47,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // MD5 加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
 
-        if (employee.getStatus() == StatusConstant.DISABLE) {
+        if (employee.getStatus().equals(StatusConstant.DISABLE)) {
             //账号被锁定
             throw new AccountLockedException(MessageConstant.ACCOUNT_LOCKED);
         }
@@ -54,4 +63,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        // 创建一个新的 Employee 实体对象，用于存储从 DTO 转换后的数据
+        Employee employee = new Employee();
+        // 使用 BeanUtils 的 copyProperties 方法将 EmployeeDTO 对象中的属性值复制到 Employee 实体对象中
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // 设置员工的状态为启用状态，StatusConstant.ENABLE 代表员工状态为启用
+        employee.setStatus(StatusConstant.ENABLE);
+        // 将默认密码进行 MD5 加密后，设置为员工的密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        // 设置员工记录的创建时间为当前时间
+        employee.setCreateTime(LocalDateTime.now());
+        // 设置员工记录的更新时间为当前时间
+        employee.setUpdateTime(LocalDateTime.now());
+        // 临时设置员工记录的创建用户 ID 为 10L，后续需要修改为当前登录用户的 ID
+        employee.setCreateUser(BaseContext.getCurrentId());
+        // 临时设置员工记录的更新用户 ID 为 10L，后续需要修改为当前登录用户的 ID
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employeeMapper.insert(employee);
+    }
 }
+
