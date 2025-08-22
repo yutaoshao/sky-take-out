e29b4229be40ab6972e258849152c99694812dcb
YutaoShao <2423271612@qq.com>
2025-08-22 18:13:02 +0800

    feat: 店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+import com.sky.constant.StatusConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author yutaoshao
+ * @description: TODO
+ * @date 2025/8/22 17:03
+ */
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> getStatus() {
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取营业状态:{}", shopStatus.equals(StatusConstant.ENABLE) ? "营业中" : "打烊中");
+        return Result.success(shopStatus);
+    }
+}
